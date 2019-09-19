c1b7d5d39422fe0f2e6d76f2828c0f7239886404
Mario Arturo Serrano Flores <maseflores79@gmail.com>
2019-09-19 14:19:57 -0500

    agregado abstract

--- Entidades/ObjetoEscuelaBase.cs
+++ Entidades/ObjetoEscuelaBase.cs
@@ -2,15 +2,12 @@ using System;
 
 namespace CoreEscuela.Entidades
 {
-    public class ObjetoEscuelaBase
+    public abstract class ObjetoEscuelaBase
     {
         public string UniqueId { get; private set; }
         public string Nombre { get; set; }
 
-        public ObjetoEscuelaBase()
-        {
-            UniqueId = Guid.NewGuid().ToString();
-        }
+        public ObjetoEscuelaBase() => UniqueId = Guid.NewGuid().ToString();
 
         public override string ToString()
         {
